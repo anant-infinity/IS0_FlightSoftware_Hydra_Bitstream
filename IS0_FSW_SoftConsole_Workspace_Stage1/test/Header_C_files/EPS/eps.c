0347d4b4824b695dfcc00951b14a9a8bae11f02d
Anant <278anant@gmai.com>
2020-02-02 14:57:46 +0800

    Completed the Logging function over the MSS UART
     Logging function doesnt include the AS7265x Data at this stage.

--- IS0_FSW_SoftConsole_Workspace_Stage1/test/Header_C_files/EPS/eps.c
+++ IS0_FSW_SoftConsole_Workspace_Stage1/test/Header_C_files/EPS/eps.c
@@ -54,146 +54,70 @@ void PWR_Switch(uint32_t config, uint8_t on_off){
 }
 
 
+
+struct GetEpsSeqPoint {
+	i2c_instance_t* i2c;
+	uint8_t slaveAddr;
+	uint8_t regAddr;
+};
+
+struct GetEpsSeqPoint points[32];
+
+struct GetEpsSeqPoint assignGetEps(i2c_instance_t* i2c, uint8_t slaveAddr, uint8_t regAddr){
+	struct GetEpsSeqPoint result;
+	result.i2c = i2c;
+	result.slaveAddr = slaveAddr;
+	result.regAddr = regAddr;
+	return result;
+}
+
+void epsAssignInit(){
+	int index = 0;
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_FG1_SLAVE_C0, EPS_FG_SOC);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH2);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH4);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC4_SLAVE_C0, EPS_VC_CH1_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH3_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH1_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC4_SLAVE_C0, EPS_VC_CH1_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH3_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH1_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH6);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH7);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH3_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH3_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH1_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH1_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH1_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH1_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH2_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH2_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH3_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH3_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH2_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH2_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH2_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH2_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH3_SHUNT_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH2_BUS_VOLTAGE);
+	points[index++] = assignGetEps(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH2_SHUNT_VOLTAGE);
+}
+
 void Get_EPS_Data(){
+	epsAssignInit();
 
 		uint8_t write_length = TX_LENGTH;
 		uint8_t rx_buffer[RX_LENGTH_2];
 		uint8_t read_length = RX_LENGTH_2;
 		i2c_status_t instance;
-
-			//Reading the Battery Voltage
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bat_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Battery Current
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bat_Curr = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the EPS Bus Voltage
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bus_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the EPS Bus Current
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH3_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bus_Curr = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//TODO: This needs to be updated
-			//Reading the EPS Battery SoC
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_FG1_SLAVE_C0, EPS_FG_SOC, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bat_SOC = Utils_Buffer_to_16_ValueU_Big(rx_buffer);;
-
-			//Reading the EPS EPS_Bat_Temp1
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH2, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bat_Temp1 = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the EPS EPS_Bat_Temp2
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH4, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bat_Temp2 = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Solar Panel (PV0) Voltage 1
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC4_SLAVE_C0, EPS_VC_CH1_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Solar Panel (PV1) Voltage 2
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH3_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Solar Panel (PV2) Voltage 3
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH1_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Solar Panel (PV0) Current 1
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC4_SLAVE_C0, EPS_VC_CH1_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Solar Panel (PV1) Current 2
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH3_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the Solar Panel (PV2) Current 3
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC3_SLAVE_C0, EPS_VC_CH1_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-
-			//TODO: This needs to be checked and confirmed that ADC can work in MODE to by default
-			//Reading the Adapter Board Temperature
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH6, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Interface_Temp = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the EPS Temperature
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_ADC_SLAVE_C1, EPS_ADC_MASK_MODE2_CH7, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_EPS_Temp = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the CIP Voltage
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH3_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the CIP Current
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH3_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the ADCS Voltage
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH1_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the ADCS Current
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH1_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the SBAND Voltage
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH1_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the SBAND Current
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH1_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the UHF Voltage
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH2_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the UHF Current
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH2_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-
-			//Reading the CDH Voltage
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH3_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the CDH Current
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH3_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_CIP_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-
-			//Reading the GPS 3V3 Voltage
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH2_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_GPS_33_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the GPS 3V3 Current
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC6_SLAVE_C1, EPS_VC_CH2_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_GPS_33_Curr = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the GPS 12 Voltage
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH2_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_GPS_12_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the GPS 12 Current
-			instance = do_write_read_transaction(&g_core_i2c1, EPS_VC5_SLAVE_C1, EPS_VC_CH2_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_GPS_12_Curr = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the EPS Battery Heater Current
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC2_SLAVE_C0, EPS_VC_CH3_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_Bat_Heater_Curr = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the DAXSS Voltage
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH2_BUS_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_DAXSS_Volt = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
-			//Reading the DAXSS Current
-			instance = do_write_read_transaction(&g_core_i2c0, EPS_VC1_SLAVE_C0, EPS_VC_CH2_SHUNT_VOLTAGE, write_length, rx_buffer, read_length);
-			Beacon_pack_IS0.EPS_DAXSS_Curr = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
-
+		for(int i = 0; i < 32; ++i){
+			instance = do_write_read_transaction(points[i].i2c, points[i].slaveAddr, points[i].regAddr, write_length, rx_buffer, read_length);
+			Beacon_pack_IS0.EPS[i] = Utils_Buffer_to_16_ValueU_Big(rx_buffer);
+		}
 
 }
 
